57c9c0e702caf3c67a9dfe2e2ab77f225e6c7489
Unknown <booster.devel@gmail.com>
2017-02-20 13:47:02 +0100

    Initial commit

--- gen-theme.sh
+++ gen-theme.sh
@@ -2,6 +2,32 @@
 themedata="/home/booster/.local/github/booster-themes"
 themename=purplerage
 
+function convertsvg {
+  for f in *.svg
+  do
+    echo Rendering ${f%%.*}.png
+    inkscape --export-background-opacity=0 --export-png=${f%%.*}.png ${f%%.*}.svg > /dev/null \
+    && optipng -o7 --quiet ${f%%.*}.png \
+    && rm ${f%%.*}.svg
+  done
+}
+
+function convertshell {
+  cd "$themedata/output/gnome-shell/3.22/assets/checkbox"
+  convertsvg
+  cd "$themedata/output/gnome-shell/3.22/assets/dash"
+  convertsvg
+  cd "$themedata/output/gnome-shell/3.22/assets/menu"
+  convertsvg
+  cd "$themedata/output/gnome-shell/3.22/assets/misc"
+  convertsvg
+  cd "$themedata/output/gnome-shell/3.22/assets/panel"
+  convertsvg
+  cd "$themedata/output/gnome-shell/3.22/assets/switch"
+  convertsvg
+}
+
+
 function compileshell {
   mkdir -p "$themedata/output/gnome-shell/3.22/"
   cp -r "$themedata/common/gnome-shell/3.22/common-assets" "$themedata/output/gnome-shell/3.22/assets-$themename"
@@ -10,6 +36,7 @@ function compileshell {
   cp -r "$themedata/common/gnome-shell/3.22/dark-assets/misc" "$themedata/output/gnome-shell/3.22/assets-$themename"
   cp -r "$themedata/common/gnome-shell/3.22/dark-assets/switch" "$themedata/output/gnome-shell/3.22/assets-$themename"
   cp -r "$themedata/common/gnome-shell/3.22/gnome-shell-dark.css" "$themedata/output/gnome-shell/3.22/gnome-shell-$themename.css"
+  convertshell
 }
 
 function compilegtk2 {
@@ -19,7 +46,7 @@ function compilegtk2 {
 
   cp -r "$themedata/common/gtk-2.0/assets/" "$themedata/output/gtk-2.0/assets-$themename"
   cp $themedata/common/gtk-2.0/*.rc "$themedata/output/gtk-2.0/"
-  cp -r "$themedata/common/gtk-2.0/gtkrc-dark" "$themedata/output/gtk-2.0/gtkrc-$themename"
+  cp -r "$themedata/common/gtk-2.0/gtkrc" "$themedata/output/gtk-2.0/gtkrc-$themename"
 }
 
 function compilegtk3 {
@@ -29,6 +56,10 @@ function compilegtk3 {
 
   cp -r "$themedata/common/gtk-3.0/3.22/assets/" "$themedata/output/gtk-3.0/3.22/assets-$themename/"
   cp -r "$themedata/common/gtk-3.0/3.22/gtk-solid-dark.css" "$themedata/output/gtk-3.0/3.22/gtk-$themename.css"
+}
+
+function compiletheme {
+  
 }
 
 rm -rf $themedata/output/
