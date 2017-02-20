d6685832db77a8da800063ffae2df0d0288f9904
Unknown <booster.devel@gmail.com>
2017-02-20 11:03:16 +0100

    Initial commit

--- common/gtk-3.0/3.22/render-assets.sh
+++ common/gtk-3.0/3.22/render-assets.sh
@@ -8,27 +8,25 @@ ASSETS_DIR="assets"
 INDEX="assets.txt"
 
 for i in `cat $INDEX`
-do 
+do
 if [ -f $ASSETS_DIR/$i.png ]; then
     echo $ASSETS_DIR/$i.png exists.
 else
-    echo
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
               --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
 fi
 if [ -f $ASSETS_DIR/$i@2.png ]; then
     echo $ASSETS_DIR/$i@2.png exists.
 else
-    echo
     echo Rendering $ASSETS_DIR/$i@2.png
     $INKSCAPE --export-id=$i \
               --export-dpi=180 \
               --export-id-only \
               --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png
 fi
 done
 exit 0
